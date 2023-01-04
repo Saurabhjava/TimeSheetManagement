76e7cea499518f8e610feeefee5d110cdabf5e1b
Saurabhjava <45967591+Saurabhjava@users.noreply.github.com>
2023-01-04 15:56:13 +0530

    Fist Commit

---
+++ TimeSheetManagment/src/main/java/com/acs/TimeSheetManagmentApplication.java
@@ -0,0 +1,13 @@
+package com.acs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeSheetManagmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimeSheetManagmentApplication.class, args);
+	}
+
+}
